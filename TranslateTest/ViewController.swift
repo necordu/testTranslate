925662eecf0f00ce7385861a79ee2a371bba3cf9
necordu <necordu_3@mail.ru>
2020-02-09 12:44:27 +0300

    inital commit

---
+++ TranslateTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TranslateTest
+//
+//  Created by Егор Редько on 09.02.2020.
+//  Copyright © 2020 necordu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
